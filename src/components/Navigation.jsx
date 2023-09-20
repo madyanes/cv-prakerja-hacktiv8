35c560987bf5deea1fd9b7a8a48c588cbb1d1e4f
madyanes <madyanes.dev@gmail.com>
2023-09-20 16:28:24 +0700

    Create site navigation

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+function Navigation() {
+  return (
+    <nav>
+      <div className='wrapper'>
+        <ul>
+          <li>
+            <a href='#'>Education</a>
+          </li>
+          <li>
+            <a href='#'>Expertise</a>
+          </li>
+          <li>
+            <a href='#'>Experience</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
