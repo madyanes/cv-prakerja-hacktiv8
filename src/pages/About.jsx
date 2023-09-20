b081580744faa5daf6f8df5df44948f478488dd4
madyanes <madyanes.dev@gmail.com>
2023-09-21 00:01:06 +0700

    Fill about page content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,22 @@
 function About() {
   return (
     <article>
-      <h1>About</h1>
-      <p>Hello world</p>
+      <header>
+        <h1>About</h1>
+      </header>
+      <p>
+        My name is Ian, a self-taught programmer living in Kediri, East Java,
+        Indonesia.
+      </p>
+      <footer>
+        <p>
+          This web application is the final project of the learning module{' '}
+          <strong>
+            &quot;Belajar Langkah Fundamental Untuk Menjadi Web Programmer&quot;
+          </strong>{' '}
+          by PT Hacktivate Teknologi Indonesia (Hacktiv8).
+        </p>
+      </footer>
     </article>
   );
 }
