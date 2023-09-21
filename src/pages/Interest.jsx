ad4c8970d84957c7a35333b83b51008bfd7ba412
madyanes <madyanes.dev@gmail.com>
2023-09-21 14:59:33 +0700

    Fill interest content

--- src/pages/Interest.jsx
+++ src/pages/Interest.jsx
@@ -1,5 +1,24 @@
 function Interest() {
-  return <h1>Interest</h1>;
+  return (
+    <article>
+      <header>
+        <h1>Interest</h1>
+      </header>
+      <p>
+        I am interested in web technologies that allow us to exchange
+        information quickly with little physical effort. Not only that, but we
+        can choose to have that information displayed in the interface we want.
+        I love coding, but am less skilled at design. Fortunately, that is the
+        domain of a UI/UX developer, not the job of a coder or web developer. So
+        I can apply my coding skills to the needs of building UIs from designs
+        provided by UI/UX developers with great interest.
+      </p>
+      <p>
+        Besides that, I also like playing online games with my friends because I
+        think it can serve as a medium to keep in touch wherever and whenever.
+      </p>
+    </article>
+  );
 }
 
 export default Interest;
