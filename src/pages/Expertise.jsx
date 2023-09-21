3e59ec99f0b16ea7c9bf468dcb94b4a06fb1a7f1
madyanes <madyanes.dev@gmail.com>
2023-09-21 11:11:44 +0700

    Display expertise as grid

--- src/pages/Expertise.jsx
+++ src/pages/Expertise.jsx
@@ -1,5 +1,68 @@
+import {
+  SiJavascript,
+  SiPython,
+  SiHtml5,
+  SiCss3,
+  SiSass,
+  SiReact,
+  SiDjango,
+  SiNextdotjs,
+} from 'react-icons/si';
+
 function Expertise() {
-  return <h1>Expertise</h1>;
+  return (
+    <article>
+      <header>
+        <h1>Expertise</h1>
+      </header>
+      <div id='section-group-grid'>
+        <section className='section-group-item li-icon-only li-bordered'>
+          <h1>Programming</h1>
+          <ul>
+            <li>
+              <SiJavascript />
+            </li>
+            <li>
+              <SiPython />
+            </li>
+          </ul>
+        </section>
+        <section className='section-group-item li-icon-only li-bordered'>
+          <h1>Markup</h1>
+          <ul>
+            <li>
+              <SiHtml5 />
+            </li>
+            <li>
+              <SiCss3 />
+            </li>
+          </ul>
+        </section>
+        <section className='section-group-item li-icon-only li-bordered'>
+          <h1>Technologies</h1>
+          <ul>
+            <li>
+              <SiReact />
+            </li>
+            <li>
+              <SiSass />
+            </li>
+          </ul>
+        </section>
+        <section className='section-group-item li-icon-only li-bordered'>
+          <h1>Frameworks</h1>
+          <ul>
+            <li>
+              <SiNextdotjs />
+            </li>
+            <li>
+              <SiDjango />
+            </li>
+          </ul>
+        </section>
+      </div>
+    </article>
+  );
 }
 
 export default Expertise;
