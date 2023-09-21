63a441d4c87025d77af42bbc409709ded152579e
madyanes <madyanes.dev@gmail.com>
2023-09-21 07:07:04 +0700

    Fill educational backgrounds content

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,5 +1,35 @@
 function Education() {
-  return <h1>Education</h1>;
+  return (
+    <article>
+      <header>
+        <h1>Educational Backgrounds</h1>
+      </header>
+      <div id='educational-backgrounds'>
+        <section className='educational-backgrounds'>
+          <h1>Formal Educations</h1>
+          <ul>
+            <li>
+              <h2>LP3I College Kediri (Diploma II)</h2>
+              <p>Majoring in Computer and Network Engineering</p>
+            </li>
+            <li>
+              <h2>Vocational High School 1 Kediri</h2>
+              <p>Majoring in Computer and Network Engineering</p>
+            </li>
+          </ul>
+        </section>
+        <section className='educational-backgrounds'>
+          <h1>Non-Formal Educations</h1>
+          <ul>
+            <li>
+              <h2>Baparekraf Digital Talent 2023</h2>
+              <p>Belajar Dasar Pemrograman Web</p>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </article>
+  );
 }
 
 export default Education;
