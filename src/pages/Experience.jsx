fd6178982c0d4c7fb5493060ae8b0960876dfc6d
madyanes <madyanes.dev@gmail.com>
2023-09-21 21:00:57 +0700

    Add work experience

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,5 +1,19 @@
 function Experience() {
-  return <h1>Experience</h1>;
+  return (
+    <article>
+      <header>
+        <h1>Work Experience</h1>
+      </header>
+      <section>
+        <h1>CV. Linksoft Mitra Informatika</h1>
+        <p>
+          I was a full-stack web developer, but mostly worked on backend
+          requirements using Python scripting language and Django framework and
+          developed Odoo modules a few times.
+        </p>
+      </section>
+    </article>
+  );
 }
 
 export default Experience;
