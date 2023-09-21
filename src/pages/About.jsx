0948924b6dd72e73813a9ccae844b788b2be1955
madyanes <madyanes.dev@gmail.com>
2023-09-21 20:07:16 +0700

    [FIX] Wrap table row (tr) with tbody

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,43 +9,45 @@ function About() {
         <h1>About</h1>
       </header>
       <table>
-        <tr>
-          <th>
-            <FaUserAlt />
-          </th>
-          <td>Madyan Eka Septian</td>
-        </tr>
-        <tr>
-          <th>
-            <FaEnvelope />
-          </th>
-          <td>
-            <a className='email' href='mailto:madyanes.dev@gmail.com'>
-              madyanes.dev@gmail.com
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <th>
-            <RiWhatsappFill />
-          </th>
-          <td>
-            <a
-              target='_blank'
-              className='whatsapp'
-              href='https://wa.me/628988889700'
-              rel='noreferrer'
-            >
-              628988889700
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <th>
-            <FaLocationDot />
-          </th>
-          <td>Kediri, East Java, Indonesia</td>
-        </tr>
+        <tbody>
+          <tr>
+            <th>
+              <FaUserAlt />
+            </th>
+            <td>Madyan Eka Septian</td>
+          </tr>
+          <tr>
+            <th>
+              <FaEnvelope />
+            </th>
+            <td>
+              <a className='email' href='mailto:madyanes.dev@gmail.com'>
+                madyanes.dev@gmail.com
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <th>
+              <RiWhatsappFill />
+            </th>
+            <td>
+              <a
+                target='_blank'
+                className='whatsapp'
+                href='https://wa.me/628988889700'
+                rel='noreferrer'
+              >
+                628988889700
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <th>
+              <FaLocationDot />
+            </th>
+            <td>Kediri, East Java, Indonesia</td>
+          </tr>
+        </tbody>
       </table>
       <footer>
         <p>
