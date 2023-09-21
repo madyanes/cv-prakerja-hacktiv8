95887ad8bf7190342f0e9a3247b170d2e486c19d
madyanes <madyanes.dev@gmail.com>
2023-09-22 05:10:43 +0700

    Move profile summary to about nav

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,6 +8,15 @@ function About() {
       <header>
         <h1>About</h1>
       </header>
+      <p className='summary'>
+        I am a passionate web technology enthusiast with a primary focus on web
+        development using JavaScript and the React library. I am also actively
+        learning TypeScript to enhance my skills in web application development.
+        Currently, I am honing my expertise in building responsive and dynamic
+        web applications. I have experience in developing various simple web
+        projects utilizing JavaScript and React, and I am dedicated to
+        continuous learning and growth in the field of web development.
+      </p>
       <table>
         <tbody>
           <tr>
