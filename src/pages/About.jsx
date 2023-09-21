8c9ea67187cee989a2a7b5c366d042dbdb683297
madyanes <madyanes.dev@gmail.com>
2023-09-21 16:50:01 +0700

    Add short information about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,52 @@
+import { FaUserAlt, FaEnvelope } from 'react-icons/fa';
+import { RiWhatsappFill } from 'react-icons/ri';
+import { FaLocationDot } from 'react-icons/fa6';
+
 function About() {
   return (
-    <article>
+    <article id='about'>
       <header>
         <h1>About</h1>
       </header>
-      <p>
-        My name is Ian, a self-taught programmer living in Kediri, East Java,
-        Indonesia.
-      </p>
+      <table>
+        <tr>
+          <th>
+            <FaUserAlt />
+          </th>
+          <td>Madyan Eka Septian</td>
+        </tr>
+        <tr>
+          <th>
+            <FaEnvelope />
+          </th>
+          <td>
+            <a className='email' href='mailto:madyanes.dev@gmail.com'>
+              madyanes.dev@gmail.com
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <th>
+            <RiWhatsappFill />
+          </th>
+          <td>
+            <a
+              target='_blank'
+              className='whatsapp'
+              href='https://wa.me/628988889700'
+              rel='noreferrer'
+            >
+              628988889700
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <th>
+            <FaLocationDot />
+          </th>
+          <td>Kediri, East Java, Indonesia</td>
+        </tr>
+      </table>
       <footer>
         <p>
           This web application is the final project of the learning module{' '}
