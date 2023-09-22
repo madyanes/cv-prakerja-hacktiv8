31626f0664acb396caa42816d93b5ce88fe48ac1
madyanes <madyanes.dev@gmail.com>
2023-09-22 17:56:37 +0700

    Apply transition entrance for Announcement

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 import Main from './components/Main';
@@ -7,11 +9,41 @@ import Announcement from './components/Announcement';
 import './App.css';
 
 function App() {
+  const [showComponent, setShowComponent] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowComponent(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
+      <style>
+        {`
+          .fade-enter {
+            opacity: 0;
+          }
+          .fade-enter-active {
+            opacity: 1;
+            transition: opacity 2000ms;
+          }
+          .fade-exit {
+            opacity: 1;
+          }
+          .fade-exit-active {
+            opacity: 0;
+            transition: opacity 500ms;
+          }
+        `}
+      </style>
       <Header />
       <Navigation />
-      <Announcement />
+      <div className={`fade-${showComponent ? 'enter' : 'exit'}-active`}>
+        {showComponent ? <Announcement /> : null}
+      </div>
       <Main />
       <Footer />
     </>
