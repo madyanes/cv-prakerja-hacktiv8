040b705882d17d6180457e2e41e3f2a582ca0c98
madyanes <madyanes.dev@gmail.com>
2023-09-23 07:50:05 +0700

    Refactor Announcement component

---
+++ src/announcements/UnderDevelopment.jsx
@@ -0,0 +1,18 @@
+function UnderDevelopment() {
+  return (
+    <>
+      <h1>Announcement</h1>
+      <p>
+        If this message appears, it means that this web is not fully finished.
+      </p>
+      <p>
+        In my current situation, completing this final project within three days
+        is impossible. But I will try to complete the main features only. Of
+        course, I will continue to develop this web considering I really need it
+        to apply for a job as a web developer.
+      </p>
+    </>
+  );
+}
+
+export default UnderDevelopment;
