7b2f4cd7df8a801e7ae6cc6ba029367038b0eba1
madyanes <madyanes.dev@gmail.com>
2023-09-27 06:34:44 +0700

    Refactor display certificates functionality

--- src/pages/Award.jsx
+++ src/pages/Award.jsx
@@ -1,10 +1,4 @@
-import certDicodingDasarPemrogramanWeb from '../assets/certificates/belajar-dasar-pemrograman-web.png';
-import certDicodingDasarPemrogramanJavascript from '../assets/certificates/belajar-dasar-pemrograman-javascript.png';
-import certDicodingBelajarMembuatFrontEndWebPemula from '../assets/certificates/belajar-membuat-front-end-web-pemula.png';
-import certDicodingBelajarAplikasiWebReact from '../assets/certificates/belajar-membuat-aplikasi-web-dengan-react.png';
-import certDicodingMemulaiPemrogramanDart from '../assets/certificates/memulai-pemrograman-dengan-dart.png';
-import certDicodingBelajarFundamentalAplikasiWebReact from '../assets/certificates/belajar-fundamental-aplikasi-web-react.png';
-import certHacktiv8BelajarFundamentalWebProgrammer from '../assets/certificates/hacktiv8-belajar-langkah-fundamental-untuk-menjadi-web-programmer.png';
+import { getCertificates } from '../utils/certificates';
 
 function Award() {
   return (
@@ -13,90 +7,13 @@ function Award() {
         <h1>Awards</h1>
       </header>
       <ul>
-        <li>
-          <a
-            target='_blank'
-            href='https://www.dicoding.com/certificates/1RXY6DW6QZVM'
-            rel='noreferrer'
-          >
-            <img
-              src={certDicodingDasarPemrogramanWeb}
-              alt='Belajar Dasar Pemrograman Web'
-            />
-          </a>
-        </li>
-        <li>
-          <a
-            target='_blank'
-            href='https://www.dicoding.com/certificates/72ZD8DWO9ZYW'
-            rel='noreferrer'
-          >
-            <img
-              src={certDicodingDasarPemrogramanJavascript}
-              alt='Belajar Dasar Pemrograman JavaScript'
-            />
-          </a>
-        </li>
-        <li>
-          <a
-            target='_blank'
-            href='https://www.dicoding.com/certificates/MRZMQR290PYQ'
-            rel='noreferrer'
-          >
-            <img
-              src={certDicodingBelajarMembuatFrontEndWebPemula}
-              alt='Belajar Membuat Front-End Web untuk Pemula'
-            />
-          </a>
-        </li>
-        <li>
-          <a
-            target='_blank'
-            href='https://www.dicoding.com/certificates/JLX1DJRVNZ72'
-            rel='noreferrer'
-          >
-            <img
-              src={certDicodingBelajarAplikasiWebReact}
-              alt='Belajar Membuat Aplikasi Web dengan React'
-            />
-          </a>
-        </li>
-        <li>
-          <a
-            target='_blank'
-            href='https://www.dicoding.com/certificates/MRZMQ03Q0PYQ'
-            rel='noreferrer'
-          >
-            <img
-              src={certDicodingMemulaiPemrogramanDart}
-              alt='Memulai Pemrograman dengan Dart'
-            />
-          </a>
-        </li>
-        <li>
-          <a
-            target='_blank'
-            href='https://www.dicoding.com/certificates/4EXGN03VQZRL'
-            rel='noreferrer'
-          >
-            <img
-              src={certDicodingBelajarFundamentalAplikasiWebReact}
-              alt='Belajar Fundamental Aplikasi Web dengan React'
-            />
-          </a>
-        </li>
-        <li>
-          <a
-            target='_blank'
-            href='https://sertiva.id/credential/b5e965a9-8caf-40c9-8662-833ffaaf31de'
-            rel='noreferrer'
-          >
-            <img
-              src={certHacktiv8BelajarFundamentalWebProgrammer}
-              alt='Belajar Langkah Fundamental Web Programmer'
-            />
-          </a>
-        </li>
+        {getCertificates().map((certificate, index) => (
+          <li key={index}>
+            <a target='_blank' href={certificate.link} rel='noreferrer'>
+              <img src={certificate.filepath} alt={certificate.name} />
+            </a>
+          </li>
+        ))}
       </ul>
     </article>
   );
---
+++ src/utils/certificates.js
@@ -0,0 +1,48 @@
+const certificates = [
+  {
+    title: 'Belajar Dasar Pemrograman Web',
+    link: 'https://www.dicoding.com/certificates/1RXY6DW6QZVM',
+    filepath: 'src/assets/certificates/belajar-dasar-pemrograman-web.png',
+  },
+  {
+    title: 'Belajar Dasar Pemrograman JavaScript',
+    link: 'https://www.dicoding.com/certificates/72ZD8DWO9ZYW',
+    filepath:
+      'src/assets/certificates/belajar-dasar-pemrograman-javascript.png',
+  },
+  {
+    title: 'Belajar Membuat Front-End Web untuk Pemula',
+    link: 'https://www.dicoding.com/certificates/MRZMQR290PYQ',
+    filepath:
+      'src/assets/certificates/belajar-membuat-front-end-web-pemula.png',
+  },
+  {
+    title: 'Belajar Membuat Aplikasi Web dengan React',
+    link: 'https://www.dicoding.com/certificates/JLX1DJRVNZ72',
+    filepath:
+      'src/assets/certificates/belajar-membuat-aplikasi-web-dengan-react.png',
+  },
+  {
+    title: 'Belajar Pemrograman Dengan Dart',
+    link: 'https://www.dicoding.com/certificates/MRZMQ03Q0PYQ',
+    filepath: 'src/assets/certificates/memulai-pemrograman-dengan-dart.png',
+  },
+  {
+    title: 'Belajar Fundamental Aplikasi Web dengan React',
+    link: 'https://www.dicoding.com/certificates/4EXGN03VQZRL',
+    filepath:
+      'src/assets/certificates/belajar-fundamental-aplikasi-web-react.png',
+  },
+  {
+    title: 'Belajar Langkah Fundamental Untuk Menjadi Web Programmer',
+    link: 'https://sertiva.id/credential/b5e965a9-8caf-40c9-8662-833ffaaf31de',
+    filepath:
+      'src/assets/certificates/hacktiv8-belajar-langkah-fundamental-untuk-menjadi-web-programmer.png',
+  },
+];
+
+function getCertificates() {
+  return certificates;
+}
+
+export { getCertificates };
